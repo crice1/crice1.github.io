e3bf0e526a24dae9d2c1f2607ebd5a617f96a1a5
crice1 <coreyrice@Coreys-MacBook-Pro.local>
2017-01-31 17:33:37 -0500

    testing push

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,3 +1,6 @@
+var varbody2 = window.varbody;
+var varicon2 = window.varicon;
+
 // Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
   // Keep the service worker alive until the notification is created.
@@ -5,8 +8,8 @@ self.addEventListener('push', function(event) {
     // There are many other possible options, for an exhaustive list see the specs:
     //   https://notifications.spec.whatwg.org/
       self.registration.showNotification('New Service Request', {
-        body: window.varbody,
-        icon: window.varicon,
+        body: varbody2,
+        icon: varicon2,
         vibrate: [200, 100, 200, 100, 200, 100, 400]
       })
   );
