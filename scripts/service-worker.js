b441349a0de9b456e16680e697834ebbee15ce51
crice1 <coreyrice@Coreys-MacBook-Pro.local>
2017-01-31 17:25:58 -0500

    push testing

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,14 +1,3 @@
-var varbody = "Fourth floor mens bathroom: the toilet in the stall is not flushing, please se...";
-var varicon = "service-request_icon.png";
-if (window.push_type == "sr"){
-  varbody = "Fourth floor mens bathroom: the toilet in the stall is not flushing, please se...";
-  varicon = "service-request_icon.png";
-}
-else{
-  varbody = "Your reservation for Large Conference Room has been approved! Click for more details.";
-  varicon = "conference-room-reservations_icon";
-}
-
 // Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
   // Keep the service worker alive until the notification is created.
@@ -16,8 +5,8 @@ self.addEventListener('push', function(event) {
     // There are many other possible options, for an exhaustive list see the specs:
     //   https://notifications.spec.whatwg.org/
       self.registration.showNotification('New Service Request', {
-        body: varbody,
-        icon: varicon,
+        body: window.varbody,
+        icon: window.varicon,
         vibrate: [200, 100, 200, 100, 200, 100, 400]
       })
   );
