b54c95fc32fd58566e4d06dbd090c4f5d540ae2a
crice1 <coreyrice@Coreys-MacBook-Pro.local>
2017-01-31 16:12:03 -0500

    push testing

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,6 +1,8 @@
 function populate_content(){
   var varbody = "Fourth floor mens bathroom: the toilet in the stall is not flushing, please se...";
   var varicon = "service-request_icon.png";
+  var data = [varbody,varicon]
+  return data;
   /*if (window.push_type == "sr"){
     varbody = "Fourth floor mens bathroom: the toilet in the stall is not flushing, please se...";
     varicon = "service-request_icon.png";
@@ -15,12 +17,12 @@ function populate_content(){
 self.addEventListener('push', function(event) {
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
-    populate_content().then(function(){
+    populate_content().then(function(data){
     // There are many other possible options, for an exhaustive list see the specs:
     //   https://notifications.spec.whatwg.org/
       self.registration.showNotification('New Service Request', {
-        body: varbody,
-        icon: varicon,
+        body: data[0],
+        icon: data[1],
         vibrate: [200, 100, 200, 100, 200, 100, 400]
       })
     })
