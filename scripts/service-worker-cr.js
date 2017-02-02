f2fd3ad9a1586da7f61c2872e9f72ecb4f8c7410
crice1 <coreyrice@Coreys-MacBook-Pro.local>
2017-02-02 14:01:46 -0500

    testing push

--- scripts/service-worker-cr.js
+++ scripts/service-worker-cr.js
@@ -4,9 +4,9 @@ self.addEventListener('push', function(event) {
   event.waitUntil(
     // There are many other possible options, for an exhaustive list see the specs:
     //   https://notifications.spec.whatwg.org/
-      self.registration.showNotification('New Service Request', {
-        body: "Fourth floor mens bathroom: the toilet in the stall is not flushing, please se...",
-        icon: "service-request_icon.png",
+      self.registration.showNotification('Reservation Approved', {
+        body: "Your reservation for Large Conference Room has been approved! Click for more details.",
+        icon: "conference-room-reservations_icon.png",
         vibrate: [200, 100, 200, 100, 200, 100, 400]
       })
   );
